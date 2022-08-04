e3b40de61ccff10054c49497f08f232db959d8f3
bigfarts <bigfarts@punymail.com>
2022-08-04 16:19:49 -0700

    add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+FLIPS=${FLIPS:-'flips'}
+
+set -x
+
+make_patch() {
+    ./make_rom.py "${1}.toml" "${2}" "${3}" "${1}-en.gba"
+    "$FLIPS" --create "${2}" "${1}-en.gba" "${1}-en.bps"
+}
+
+make_patch exe4bm10 bn4bm.gba exe4bm10.gba
+make_patch exe4rs11 bn4rs.gba exe4rs11.gba
